1d779787055a831368195755eb86d84ce7c884ce
Matthew Gordon <laxredemption5525@gmail.com>
2016-11-07 20:19:42 -0700

    Finished Ajax functionality

--- js/app.js
+++ js/app.js
@@ -2,7 +2,8 @@
   'use strict';
 
   var movies = [];
-
+  var submitBtn = $("#searchSubmit");
+  var search = $('#search');
   var renderMovies = function() {
     $('#listings').empty();
 
@@ -14,17 +15,17 @@
 
       $title.attr({
         'data-position': 'top',
-        'data-tooltip': movie.title
+        'data-tooltip': movie.Title
       });
 
       $title.tooltip({ delay: 50, });
-      $title.text(movie.title);
+      $title.text(movie.Title);
 
       var $poster = $('<img class="poster">');
 
       $poster.attr({
-        src: movie.poster,
-        alt: `${movie.poster} Poster`
+        src: movie.Poster,
+        alt: `${movie.Poster} Poster`
       });
 
       $content.append($title, $poster);
@@ -33,7 +34,7 @@
       var $action = $('<div class="card-action center">');
       var $plot = $('<a class="waves-effect waves-light btn modal-trigger">');
 
-      $plot.attr('href', `#${movie.id}`);
+      $plot.attr('href', `#${movie.Id}`);
       $plot.text('Plot Synopsis');
 
       $action.append($plot);
@@ -41,8 +42,8 @@
 
       var $modal = $(`<div id="${movie.id}" class="modal">`);
       var $modalContent = $('<div class="modal-content">');
-      var $modalHeader = $('<h4>').text(movie.title);
-      var $movieYear = $('<h6>').text(`Released in ${movie.year}`);
+      var $modalHeader = $('<h4>').text(movie.Title);
+      var $movieYear = $('<h6>').text(`Released in ${movie.Year}`);
       var $modalText = $('<p>').text(movie.plot);
 
       $modalContent.append($modalHeader, $movieYear, $modalText);
@@ -53,8 +54,26 @@
       $('#listings').append($col);
 
       $('.modal-trigger').leanModal();
+      console.log(movie.Title);
     }
   };
 
-  // ADD YOUR CODE HERE
+  // Listen for submissions on the search form. Remember to prevent the default action.
+
+  submitBtn.click(function(e){
+    e.preventDefault();
+    if (!search.val()) {
+      Materialize.toast('Please enter a movie title.', 4000);
+    } else {
+       $.getJSON('http://www.omdbapi.com/?s=' + search.val(), function(data){
+        for ( var movie in data.Search ) {
+          movies.push(data.Search[movie]);
+        }
+        renderMovies();
+        search.val('');
+        movies = [];
+      });
+    }
+  });
+
 })();
